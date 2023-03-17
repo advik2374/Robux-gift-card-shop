8180b5247909dd76239e0e04781cdd59bd0800da
ADVIK <127037729+advik2374@users.noreply.github.com>
2023-03-17 17:59:00 +0000

    Update class1.js

--- class1.js
+++ class1.js
@@ -1,5 +1,10 @@
 function pageLoad(){
 alert("Hello")
 var a=prompt("Enter your name")
+if(a==""){
+    alert("Hello NaN you can get robux today")
+}
+else{
 alert("hello "+a+" you can get robux today")
+}
 }
